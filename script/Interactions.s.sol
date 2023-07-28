58fb511808c9ed0e95c1336b0f6acca951cc7a40
wildkraken <soufiansabni@gmail.com>
2023-07-29 00:06:50 +0200

    My first commit

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -7,6 +7,10 @@ import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
 
 contract FunFundMe is Script {
     function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe", 
+            block.chainid
+        );
 
     }
 
